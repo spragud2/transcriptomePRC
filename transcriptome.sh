9b605fa6484f531f2e3bcdc0262b1ae469e88891
Daniel Sprague <dsprague@longleaf-login2.its.unc.edu>
2019-05-02 12:15:24 -0400

    pickles

---
+++ transcriptome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH -n 24
+#SBATCH --mem=2g
+#SBATCH -t 5-00:00:00
+
+for f in *.fa; do sbatch -J $f -N 1 -n 24 -t 50:00:00 --wrap="python qSEEKRscanr.py  -t $f -k 4"; done
