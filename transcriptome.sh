deaaf3678f8aafc45993825fda4479e1dc22fb4f
Daniel Sprague <dsprague@longleaf-login2.its.unc.edu>
2019-05-06 18:25:15 -0400

    results

--- transcriptome.sh
+++ transcriptome.sh
@@ -6,4 +6,4 @@
 #SBATCH --mem=2g
 #SBATCH -t 5-00:00:00
 
-for f in *.fa; do sbatch -J $f -N 1 -n 24 -t 50:00:00 --wrap="python qSEEKRscanr.py  -t $f -k 4"; done
+for f in *.fa; do sbatch -J $f -N 1 -n 24 -t 50:00:00 --wrap="python qSEEKRscanr.py  -t $f -k 2 -w 100 -s 20"; done
