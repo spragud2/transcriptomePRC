5a8a9f1b7d84257f1858626b8d61147f72c2d390
Daniel Sprague <danielsprague@Daniels-MacBook-Pro.local>
2019-05-06 17:37:35 -0400

    markov update

--- qSEEKRscanr.py
+++ qSEEKRscanr.py
@@ -19,39 +19,43 @@ from tqdm import tqdm
 
 ###########################################################################
 
+#
+# def rectCorr(queries_kmers,ref_kmers):
+#     queries_kmers = (queries_kmers.T - np.mean(queries_kmers, axis=1)).T
+#     ref_kmers = (ref_kmers.T - np.mean(ref_kmers, axis=1)).T
+#     #Transpose tile matrix and dot product queries against tiles
+#     cov = queries_kmers.dot(ref_kmers.T)
+#     #Get the euclidean norm of each query and tile
+#     qNorm = np.linalg.norm(queries_kmers, axis=1)
+#     tNorm = np.linalg.norm(ref_kmers, axis=1)
+#     #Outer vector multiplication to match query to tiles
+#     norm = np.outer(qNorm, tNorm)
+#     #Correlation matrix calculation
+#     qSEEKRmat = cov/norm
+#     return qSEEKRmat.T
+def classify(seq, k, lrTab):
+    """ Classify seq using given log-ratio table.  We're ignoring the
+        initial probability for simplicity. """
+    seq = seq.upper()
+    bits = 0
+    nucmap = { 'A':0, 'T':1, 'C':2, 'G':3 }
+    rowmap = dict(zip([''.join(p) for p in itertools.product(bases,repeat=k-1)],range(4**(k-1))))
+    for kmer in [seq[i:i+k] for i in range(len(seq)-k+1) ]:
+        i, j = rowmap[kmer[:k-1]], nucmap[kmer[-1]]
+        bits += lrTab[i, j]
+    return bits
 
-def rectCorr(queries_kmers,ref_kmers):
-    queries_kmers = (queries_kmers.T - np.mean(queries_kmers, axis=1)).T
-    ref_kmers = (ref_kmers.T - np.mean(ref_kmers, axis=1)).T
-    #Transpose tile matrix and dot product queries against tiles
-    cov = queries_kmers.dot(ref_kmers.T)
-    #Get the euclidean norm of each query and tile
-    qNorm = np.linalg.norm(queries_kmers, axis=1)
-    tNorm = np.linalg.norm(ref_kmers, axis=1)
-    #Outer vector multiplication to match query to tiles
-    norm = np.outer(qNorm, tNorm)
-    #Correlation matrix calculation
-    qSEEKRmat = cov/norm
-    return qSEEKRmat.T
-
-
-def qSEEKR(refs, k, Q, target, w, s,mean,std,k_map):
+def qSEEKR(k,ae4Tbl,bTbl, target, w, s):
     t_h, t_s = target
     window, slide = w, s
     hits = {}
     tiles = [t_s[i:i+window] for i in range(0, len(t_s), slide)]
-    threeprime_hang = len(t_s) % slide
-    if threeprime_hang != 0:
-       tiles[-1] = tiles[-1]+t_s[-threeprime_hang:]
 
-    tCounts = BasicCounter(k=k,mean=mean,std=std,silent=True)
-    tCounts.seqs = tiles
-    tCounts.get_counts()
-    #Completely vectorized implementation of the old 'dSEEKR'
-    #Convert row means in matrices to 0
-    qSEEKRmat = rectCorr(Q,tCounts.counts)
-    hits_idx = np.argwhere(qSEEKRmat > refs)
-    tot_scores = np.sum(qSEEKRmat > refs) / len(t_s)
+    bSeq = np.array([classify(tile,k,bTbl) for tile in tiles])
+    ae4Seq = np.array([classify(tile,k,ae4Tbl) for tile in tiles])
+    qSEEKRmat = np.column_stack((bSeq,ae4Seq))
+    hits_idx = np.argwhere(qSEEKRmat > 0,axis=0)
+    tot_scores = np.sum(qSEEKRmat > 0,axis=0) / len(t_s)
     return t_h, [hits_idx, tot_scores]
 ###########################################################################
 
@@ -74,41 +78,15 @@ kmer_map = dict(zip(kmers,range(0,4**args.k)))
 
 ###########################################################################
 #Path to known functional domains
-query_path = './queries/queries.fa'
-target_path = args.t
-target_head, target_seq = Reader(
-    target_path).get_headers(), Reader(target_path).get_seqs()
-target_dict = dict(zip(target_head, target_seq))
 
-queries = dict(zip(Reader(query_path).get_headers(),
-                   Reader(query_path).get_seqs()))
+b_model = np.load('./bmodel_2mers.mkv.npy')
+ae4_model = np.load('./AE4model_2mers.mkv.npy')
+lncome_model = np.load('./genomemodel_2mers.mkv.npy')
 ###########################################################################
 
 ###########################################################################
-mean_paths = [f for f in glob.iglob('./stats/*mean.npy')]
-std_paths = [f for f in glob.iglob('./stats/*std.npy')]
-
-means = {}
-for mean_path in mean_paths:
-    means[basename(mean_path)] = np.load(mean_path)
-stds = {}
-for std_path in std_paths:
-    stds[basename(std_path)] = np.load(std_path)
-mean = means[f'{args.k}mean.npy']
-std = stds[f'{args.k}std.npy']
-###########################################################################
-
-###########################################################################
-ref = np.load(f'./refs/{args.k}ref.npy')
-###########################################################################
-
-###########################################################################
-
-queries = BasicCounter(infasta='./queries/queries.fa',k=args.k,mean=mean,std=std,silent=True)
-queries.get_counts()
-Q = queries.counts
-
-percentiles = np.percentile(ref, args.thresh, axis=0)
+bTbl = np.log2(b_model) - np.log2(lncome_model)
+ae4Tbl = np.log2(ae4_model) - np.log2(lncome_model)
 ###########################################################################
 '''
 Parallelize transcript computations
@@ -116,7 +94,7 @@ Parallelize transcript computations
 ###########################################################################
 with multiprocessing.Pool(args.n) as pool:
     ha = pool.starmap(qSEEKR, product(
-        *[[percentiles], [args.k], [Q], list(target_dict.items()), [args.w], [args.s],[mean],[std],[kmer_map]]))
+        *[[args.k], [ae4Tbl], [bTbl],list(target_dict.items()), [args.w], [args.s]]))
     hits = dict(ha)
-
+print(hits)
 pickle.dump(hits, open(f'../{basename(args.t)[:-3]}_{args.k}_{args.thresh}_{args.w}win_{args.s}slide_scores.p', 'wb'))
