40981d01cce0f96f1df53f83073af1d31d4b9e21
Daniel Sprague <danielsprague@Daniels-MacBook-Pro.local>
2019-05-07 13:33:56 -0400

    update

--- qSEEKRscanr.py
+++ qSEEKRscanr.py
@@ -58,8 +58,7 @@ def qSEEKR(k,ae4Tbl,bTbl, target, w, s,thresh):
     hits_idx = np.argwhere(qSEEKRmat > thresh)
     tot_scores = np.sum(qSEEKRmat > thresh,axis=0) / len(t_s)
     tot_scores = np.sum(tot_scores)
-    negtot = (qSEEKRmat <= thresh).sum() / len(tiles)
-    return t_h, [hits_idx, tot_scores,negtot]
+    return t_h, [hits_idx, tot_scores,qSEEKRmat]
 ###########################################################################
 
 ###########################################################################
